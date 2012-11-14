1d2772dfe85189f747cc2ba1c78b9c64e5b9a9ca
Richard Towers <richard_towers@Richard-Towers-Macbook.local>
2012-11-14 21:48:44 +0000

    Fixing regex
    Obviously this is a ridiculous approach!

--- compiler.js
+++ compiler.js
@@ -1,5 +1,12 @@
-function ((decaf) {
+(function (decaf) {
+  	'use strict';
 	decaf.Compile = function (source) {
-		source.replace(/(([^)]*))function( ?)/g, function (_, arguments, whitespace) { return 'function' + whitespace + '(' + arguments + ')'; });
-	}
+		// We want to replace '(...) ->' with 'function ()'
+		var regex = /\(([^\(\)\\]*?)\)(\s*)->(\s*)/g;
+		return source.replace(
+			regex,
+			function (_, args, whitespaceBefore, whitespaceAfter) { 
+				return 'function' + whitespaceAfter + '(' + args + ')';
+			});
+	};
 }(window.Decaf = window.Decaf || {}));
